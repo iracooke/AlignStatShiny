2d16b55e5c64c426b9d0ed5746a402dd6f06f148
Ira Cooke <iracooke@gmail.com>
2015-12-08 14:36:31 +1100

    Initial basic working web app for AlignStat

---
+++ server.R
@@ -0,0 +1,89 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(AlignStat)
+library(ggplot2)
+
+shinyServer(function(input, output) {
+
+  # Compute alignment comparison as a reactive object
+  # This only gets run if a different alignment gets uploaded
+  #
+  comparison <- reactive({
+    aa_file <- input$align_a
+    ab_file <- input$align_b
+    if (is.null(aa_file) | (is.null(ab_file)))
+      return(NULL)
+    
+    aa_path <- aa_file$datapath
+    ab_path <- ab_file$datapath
+    
+    compare_alignments(aa_path,ab_path)
+  })
+  
+  
+  
+  output$match_summary <- renderPlot({
+    if (is.null(comparison()))
+      return(NULL)
+
+    p <- plot_match_summary(comparison(),cys = input$show_prop_cys,display = FALSE)
+    p <- p + ggtitle("Match Summary") + theme(title = element_text(size=20))
+    p
+  })
+  
+  output$match_summary_caption <- renderText({
+    if (is.null(comparison()))
+      return(NULL)
+
+    "Proportion of alignments with identical character matches (Identity) as
+    a function of alignment position. Optionally also plots the proportion
+    of cysteines (Cysteines) at each position."
+  })
+
+  
+  output$heatmap <- renderPlot({
+    if (is.null(comparison()))
+      return(NULL)
+    
+    p <- plot_alignment_heatmap(comparison(),display = FALSE)
+    p <- p + ggtitle("Agreement Heatmap") + theme(title = element_text(size=20))
+    p <- p + xlab("Alignment A") + ylab("Alignment B")
+    p
+  })
+  
+  output$heatmap_caption <- renderText({
+    if (is.null(comparison()))
+      return(NULL)
+    
+    "Agreement heatmap. The value at point (x,y) represents the agreement between 
+    position x in alignment A and position y in alignment B. Agreement value is computed 
+    as the proportion of identical matches not counting conserved gaps. Use this plot to 
+    determine which positions are well agreed upon by the MSAs, and which are split by 
+    one MSA relative to the other."
+  })
+  
+  output$category_proportions <- renderPlot({
+    if (is.null(comparison()))
+      return(NULL)
+    
+    p <- plot_category_proportions(comparison(),stack = input$stack_category_proportions,display = FALSE)
+    p <- p + ggtitle("Categorized Differences") + theme(title= element_text(size=20))
+    p
+  })
+  
+  output$category_proportions_caption <- renderText({
+    if (is.null(comparison()))
+      return(NULL)
+    
+    "Detailed breakdown of the differences between the multiple 
+    sequence alignments. For all characters that are neither identical residues, nor conserved 
+    gaps, the relative proportions of insertions (I), deletions (D) and substitutions (S) is plotted."    
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,40 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  
+
+  # Sidebar with a slider input for number of bins
+  verticalLayout(
+    titlePanel("AlignStat: Compare two alignments of the same sequences"),
+    wellPanel(
+      h3("Upload your alignments to make a comparison"),
+      p("Alignments should be of the same sequences in the same order"),
+      
+      fileInput("align_a","Alignment A in FASTA format"),
+      fileInput("align_b","Alignment B in FASTA format"),
+      checkboxInput("show_prop_cys","Show Cysteine Proportions",value = TRUE),
+      checkboxInput("stack_category_proportions","Stack Category Proportions")
+    ),
+        
+    # Show a plot of the generated distribution
+    plotOutput("match_summary"),
+    textOutput("match_summary_caption"),
+    br(),br(),
+
+    plotOutput("heatmap"),
+    textOutput("heatmap_caption"),
+    br(),br(),
+    
+    plotOutput("category_proportions"),
+    textOutput("category_proportions_caption"),
+    br()
+  )
+))
