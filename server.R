93c2d7f592aeb6d58bd6fc1c98054eec2096b9bd
TS404 <TS404@users.noreply.github.com>
2016-01-07 15:23:38 +1100

    + plot_dissimilarity_matrix

--- server.R
+++ server.R
@@ -30,66 +30,90 @@ shinyServer(function(input, output) {
   
   
   
-  output$match_summary <- renderPlot({
-    if (is.null(comparison()))
-      return(NULL)
-
-    p <- plot_match_summary(comparison(),cys = input$show_prop_cys,display = FALSE)
-    p <- p + ggtitle("Match Summary") + theme(title = element_text(size=20))
-    p
-  })
-  
-  output$match_summary_caption <- renderText({
-    if (is.null(comparison()))
-      return(NULL)
-
-    "Proportion of alignments with identical character matches (Identity) as
-    a function of alignment column."
-    
-    if (is.null(show_prop_cys()))
-      return(NULL)
-    
-    "Additionally, proportion of cysteines at each alignmebt position."
-  })
-
-  
   output$heatmap <- renderPlot({
     if (is.null(comparison()))
       return(NULL)
     
-    p <- plot_alignment_heatmap(comparison(),display = FALSE)
-    p <- p + ggtitle("Agreement Heatmap") + theme(title = element_text(size=20))
-    p <- p + xlab("Alignment A") + ylab("Alignment B")
+    p <- plot_similarity_heatmap(comparison(),display = FALSE)
+    p <- p + ggtitle("Similarity Heatmap") + theme(title = element_text(size=20))
+    p <- p + xlab("Alignment A columns") + ylab("Alignment B columns")
     p
   })
-  
   output$heatmap_caption <- renderText({
     if (is.null(comparison()))
       return(NULL)
     
-    "Agreement heatmap. The value at point (x,y) represents the agreement between 
-    column x in alignment A and column y in alignment B. Agreement value is computed 
-    as the proportion of identical matches (not counting conserved gaps). Use this plot to 
-    determine which positions are well agreed upon by the MSAs, and which are split by 
-    one MSA relative to the other."
+    "Similarity heatmap. The value at point (x,y) represents the similarity between 
+    column x in alignment A and column y in alignment B. Similarity value is computed 
+    as the proportion of identical characters (excluding conserved gaps). This plot
+    indicates which positions are well agreed upon by the MSAs, and which are split
+    by one MSA relative to the other."
   })
   
-  output$category_proportions <- renderPlot({
+  
+  
+  output$matrix <- renderPlot({
+    if (is.null(comparison()))
+      return(NULL)
+    
+    p <- plot_similarity_heatmap(comparison(),display = FALSE)
+    p <- p + ggtitle("Dissimilarity matrix") + theme(title = element_text(size=20))
+    p <- p + xlab("Alignment A columns") + ylab("Sequence")
+    p
+  })
+  output$matrix_caption <- renderText({
+    if (is.null(comparison()))
+      return(NULL)
+    
+    "Dissimilarity matrix plot. For each residue in alignment A, colour indicates the
+    dissimilariyof alignment B (match, merge, split, shift, or conserved gap). This
+    plot indicates of which sequence regions are most disagreed upon by the MSAs."
+  })
+  
+  
+  
+  output$match_summary <- renderPlot({
+    if (is.null(comparison()))
+      return(NULL)
+
+    p <- plot_similarity_summary(comparison(),cys = input$show_prop_cys,display = FALSE)
+    p <- p + ggtitle("Similarity Summary") + theme(title = element_text(size=20))
+    p <- p + xlab("Alignment A columns")
+    p
+  })
+  output$match_summary_caption <- renderText({
+    if (is.null(comparison()))
+      return(NULL)
+
+    "Summary of the similarity between the multiple sequence alignments. For each
+    column of alignment A, the proportion of identical characters to alignment B is
+    plotted, normalised to the proportion of characters that are not conserved gaps."
+    
+    if (is.null(show_prop_cys()))
+      return(NULL)
+    
+    "Additionally, proportion of cysteines at each alignment column is shown."
+  })
+
+
+  
+  output$plot_dissimilarity_summary <- renderPlot({
     if (is.null(comparison()))
       return(NULL)
     
     p <- plot_category_proportions(comparison(),stack = input$stack_category_proportions,display = FALSE)
-    p <- p + ggtitle("Categorized Differences") + theme(title= element_text(size=20))
+    p <- p + ggtitle("Dissimilarity summary") + theme(title= element_text(size=20))
+    p <- p + xlab("Alignment A columns")
     p
   })
-  
   output$category_proportions_caption <- renderText({
     if (is.null(comparison()))
       return(NULL)
     
-    "Detailed breakdown of the differences between the multiple 
-    sequence alignments. For all characters that are neither identical residues, nor conserved 
-    gaps, the relative proportions of insertions (I), deletions (D) and substitutions (S) is plotted."    
+    "Summary of the dissimilarities between the multiple sequence alignments. For
+    each column of alignment A, the relative proportions of merges, splits and
+    shifts is plotted, normalised to the proportion of characters that are not
+    conserved gaps."
   })
   
 })
