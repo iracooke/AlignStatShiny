abb0489b8be45bdecab5916ab15046e40f802da0
Thomas Shafee <TS404@users.noreply.github.com>
2016-05-12 14:39:53 +1000

    Add download buttons for plots

--- server.R
+++ server.R
@@ -5,8 +5,11 @@
 # http://shiny.rstudio.com
 #
 
+
 library(devtools)
 devtools::install_github("TS404/AlignStat")
+
+# library(AlignStat) # <<<<------- restore once AlignStat updated on CRAN
 
 library(shiny)
 library(AlignStat)
@@ -29,15 +32,29 @@ shinyServer(function(input, output) {
     compare_alignments(aa_path,ab_path)
   })
   
+  output$comparison_done <- reactive({
+    return(!is.null(comparison()))
+  })
+  outputOptions(output, 'comparison_done', suspendWhenHidden=FALSE)
   
+  output$example_data_download <- downloadHandler(filename = "AlignStatExample.zip",content = function(file){
+        download.file(url = "https://dl.dropboxusercontent.com/u/226794/AlignStatShiny/example.zip",destfile = file,quiet = TRUE)
+  })
   
-  output$heatmap <- renderPlot({
-    if (is.null(comparison()))
-      return(NULL)
-    
+  #
+  # Heatmap 
+  #
+  
+  plot_heatmap <- function(){
     p <- plot_similarity_heatmap(comparison(),display = FALSE)
     p <- p + ggtitle("Similarity Heatmap") + theme(title = element_text(size=20))
     p <- p + xlab("Alignment A columns") + ylab("Alignment B columns")
+    p
+  }
+  output$heatmap <- renderPlot({
+    if (is.null(comparison()))
+      return(NULL)
+    p <- plot_heatmap()
     p
   })
   output$heatmap_caption <- renderText({
@@ -50,16 +67,25 @@ shinyServer(function(input, output) {
     indicates which positions are well agreed upon by the MSAs, and which are split
     by one MSA relative to the other."
   })
+  output$heatmap_download <- downloadHandler(filename = "heatmap.pdf",content = function(file){
+    p <- plot_heatmap()
+    ggsave(filename = file,plot = p,device = "pdf",width = 10,height = 6)
+  })
   
+  #
+  # Matrix
+  # 
   
-  
+  plot_matrix <- function(){
+    p <- plot_dissimilarity_matrix(comparison(),display = FALSE)
+    p <- p + ggtitle("Dissimilarity matrix") + theme(title = element_text(size=20))
+    p <- p + xlab("Alignment A columns") + ylab("Sequence")
+    p    
+  }
   output$matrix <- renderPlot({
     if (is.null(comparison()))
       return(NULL)
-    
-    p <- plot_similarity_heatmap(comparison(),display = FALSE)
-    p <- p + ggtitle("Dissimilarity matrix") + theme(title = element_text(size=20))
-    p <- p + xlab("Alignment A columns") + ylab("Sequence")
+    p <- plot_matrix()
     p
   })
   output$matrix_caption <- renderText({
@@ -70,16 +96,25 @@ shinyServer(function(input, output) {
     dissimilariyof alignment B (match, merge, split, shift, or conserved gap). This
     plot indicates of which sequence regions are most disagreed upon by the MSAs."
   })
+  output$matrix_download <- downloadHandler(filename = "matrix.pdf",content = function(file){
+    p <- plot_matrix()
+    ggsave(filename = file,plot = p,device = "pdf",width = 10,height = 6)
+  })  
   
+  #
+  # Match Summary
+  #
   
-  
-  output$match_summary <- renderPlot({
-    if (is.null(comparison()))
-      return(NULL)
-
+  plot_match_summary <- function(){
     p <- plot_similarity_summary(comparison(),cys = input$show_prop_cys,display = FALSE)
     p <- p + ggtitle("Similarity Summary") + theme(title = element_text(size=20))
     p <- p + xlab("Alignment A columns")
+    p    
+  }
+  output$match_summary <- renderPlot({
+    if (is.null(comparison()))
+      return(NULL)
+    p <- plot_match_summary()
     p
   })
   output$match_summary_caption <- renderText({
@@ -90,24 +125,33 @@ shinyServer(function(input, output) {
     column of alignment A, the proportion of identical characters to alignment B is
     plotted, normalised to the proportion of characters that are not conserved gaps."
     
-    if (is.null(show_prop_cys()))
+    if (input$show_prop_cys)
       return(NULL)
     
     "Additionally, proportion of cysteines at each alignment column is shown."
   })
+  output$match_summary_download <- downloadHandler(filename = "match_summary.pdf",content = function(file){
+    p <- plot_match_summary()
+    ggsave(filename = file,plot = p,device = "pdf",width = 10,height = 6)
+  })  
 
+  #
+  # Dissimilarity Summary
+  #
 
-  
+  plot_diss_sum <- function(){
+    p <- plot_dissimilarity_summary(comparison(),stack = input$stack_category_proportions,display = FALSE)
+    p <- p + ggtitle("Dissimilarity summary") + theme(title= element_text(size=20))
+    p <- p + xlab("Alignment A columns")
+    p    
+  }  
   output$plot_dissimilarity_summary <- renderPlot({
     if (is.null(comparison()))
       return(NULL)
-    
-    p <- plot_category_proportions(comparison(),stack = input$stack_category_proportions,display = FALSE)
-    p <- p + ggtitle("Dissimilarity summary") + theme(title= element_text(size=20))
-    p <- p + xlab("Alignment A columns")
+    p <- plot_diss_sum()
     p
   })
-  output$category_proportions_caption <- renderText({
+  output$plot_dissimilarity_caption <- renderText({
     if (is.null(comparison()))
       return(NULL)
     
@@ -116,29 +160,42 @@ shinyServer(function(input, output) {
     shifts is plotted, normalised to the proportion of characters that are not
     conserved gaps."
   })
+  output$dissimilarity_summary_download <- downloadHandler(filename = "dissimilarity_summary.pdf",content = function(file){
+    p <- plot_diss_sum()
+    ggsave(filename = file,plot = p,device = "pdf",width = 10,height = 6)
+  })  
   
+  #
+  # Sum of pairs
+  #
   
-  output$SP_summary <- renderPlot({
-    if (is.null(comparison()))
-      return(NULL)
-    if (is.null(sum_of_pairs()))
-      return(NULL)  
-
-    p <- plot_SP_summary(comparison(),display = FALSE)
-    p <- p + ggtitle("Sum of Pairs Summary") + theme(title = element_text(size=20))
-    p <- p + xlab("Alignment B columns")
-    p
-  })
-  output$SP_summary_caption <- renderText({
-    if (is.null(comparison()))
-      return(NULL)
-    if (is.null(sum_of_pairs()))
-      return(NULL)
-    
+  plot_SP_summary <- function(){
+      p <- plot_SP_summary(comparison(),display = FALSE)
+      p <- p + ggtitle("Sum of Pairs Summary") + theme(title = element_text(size=20))
+      p <- p + xlab("Alignment B columns")
+      p
+    }
+    output$SP_summary <- renderPlot({
+      if (is.null(comparison()))
+        return(NULL)
+      if (input$sum_of_pairs)
+        return(NULL)
+      p <- plot_match_summary()
+      p
+    })
+    output$SP_summary_caption <- renderText({
+      if (is.null(comparison()))
+        return(NULL)
+      if (input$sum_of_pairs)
+        return(NULL)
+  
     "Summary of the sum of pairs score and related scores between the multiple sequence
     alignments. The sum of pairs is the proportion of aligned pairs from alignment A
-    that are retained in alignment B. The column score is the proportion of columns from
-    alignment A that are fully retained in alignment B."
-  })
-  
+    that are retained in alignment B. The column score is the proportion of columns
+    from alignment A that are fully retained in alignment B."
+    })
+    output$plot_SP_summary <- downloadHandler(filename = "SP_summary.pdf",content = function(file){
+      p <- plot_SP_summary()
+      ggsave(filename = file,plot = p,device = "pdf",width = 10,height = 6)
+    })
 })
